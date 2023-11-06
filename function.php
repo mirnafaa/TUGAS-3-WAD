b16072425f443067effc7cdf0e1cf4f639ad38a5
mirnafaa <115998619+mirnafaa@users.noreply.github.com>
2023-11-06 08:33:25 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,64 @@
+<?php
+/*CREATE TABLE daftar_barang (
+    no int PRIMARY KEY AUTO_INCREMENT,
+    kodebarang varchar(10),
+    namabarang varchar(50),
+    gambar varchar(50),
+    hargajual int,
+    stokbarang int
+);*/
+
+$conn = mysqli_connect("localhost:8080","root","","tugaswad3");
+
+function query($query){
+    global $conn;
+    $result = mysqli_query($conn,$query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)){
+        $rows [] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data){
+    global $conn;
+    $kodebarang = $data['kodebarang'];
+    $namabarang = $data['namabarang'];
+    $gambar = $data['gambarbarang'];
+    $hargajual = $data['hargajual'];
+    $stokbarang = $data['stokbarang'];
+
+    $query = "INSERT INTO daftar_barang
+        VALUES
+        ('','$kodebarang','$namabarang','$gambar','$hargajual','$stokbarang')";
+    mysqli_query($conn,$query);
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($no){
+    global $conn;
+    mysqli_query($conn, "DELETE FROM daftar_barang WHERE no = $no");
+    return mysqli_affected_rows($conn);
+}
+
+function ubah($data){
+    global $conn;
+    $no = $data["no"];
+    $kodebarang = $data['kodebarang'];
+    $namabarang = $data['namabarang'];
+    $gambar = $data['gambarbarang'];
+    $hargajual = $data['hargajual'];
+    $stokbarang = $data['stokbarang'];
+
+    $query = "UPDATE daftar_barang SET
+        kodebarang = '$kodebarang',
+        namabarang = '$namabarang',
+        gambar = '$gambar',
+        hargajual = '$hargajual',
+        stokbarang = '$stokbarang'
+        WHERE no = $no
+    ";
+    mysqli_query($conn,$query);
+    return mysqli_affected_rows($conn);
+}
+?>
